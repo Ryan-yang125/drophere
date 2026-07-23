5a5ebf2a1b8e21fa293712ef83122ac3c19659d1
Ryan-yang125 <ruiyang0012@gmail.com>
2026-07-24 01:34:13 +0800

    release: prepare v1.0.1

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -1,7 +1,7 @@
 import os from "node:os";
 import path from "node:path";
 
-export const VERSION = "1.0.0";
+export const VERSION = "1.0.1";
 export const DEFAULT_API = "https://api.drophere.page";
 export const DEFAULT_BASE_DOMAIN = "drophere.page";
 export const USER_CONFIG_PATH = path.join(os.homedir(), ".config", "drophere", "config.json");
